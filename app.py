2185096b5cfc0481da0172a760d743050d73382c
Michael Timbs <mtimbs91@gmail.com>
2019-09-18 23:38:05 +0930

    move basic KNN run to baseline function

--- app.py
+++ app.py
@@ -4,12 +4,18 @@ from Models.knn import knn
 from Helpers.visualisations import  plot_confusion_matrix
 from PreProcessing.load_data import all_data_without_contested_possession_breakdown
 
-features, targets = all_data_without_contested_possession_breakdown()
 
-X_train, X_test, y_train, y_test = train_test_split(features, targets, test_size=0.3, random_state=0)
+def knn_baseline():
+    '''
+    Basic implementation of KNN algorithm to get a baseline confusion matrix for brownlow predictions.
+    '''
+    features, targets = all_data_without_contested_possession_breakdown()
 
-knn_model = knn(X_train, y_train, 10)
+    X_train, X_test, y_train, y_test = train_test_split(features, targets, test_size=0.3, random_state=0)
 
-predictions = knn_model.predict(X_test)
+    knn_model = knn(X_train, y_train, 10)
 
-plot_confusion_matrix(y_test, predictions, 'KNN - No sampling, 10 Neighbours')
+    predictions = knn_model.predict(X_test)
+
+    plot_confusion_matrix(y_test, predictions, 'KNN - No sampling, 10 Neighbours')
+
